debbcc77b0630dbd3a30ea764de0581c8b3c7b47
tsoumagas-benjamin <bptsoumagas@gmail.com>
2021-04-05 16:19:23 -0400

    Added icon, readme, gitignore

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,7 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Rock Paper Scissors");
+
+        Image icon = new Image("file:icon.jpg");
+        primaryStage.getIcons().add(icon);
+
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
