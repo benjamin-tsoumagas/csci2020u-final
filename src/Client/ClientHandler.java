43d6d6774057206f8bbd75367bc88304620aaef2
Matthew_Sharp <matthew.sharp1@ontariotechu.net>
2021-04-07 23:57:26 -0400

    Added Server and Handler

---
+++ src/Client/ClientHandler.java
@@ -0,0 +1,134 @@
+package Client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.*;
+
+public class ClientHandler extends  Thread{
+    protected Socket socket = null; // client socket
+    protected PrintWriter out = null; // used to write to the socket
+    protected BufferedReader in = null; // used to read from the socket
+    protected Vector moves = null; // used to store the files in the shared directory
+    protected int currentClients = 0;
+    protected String strUSR = null;
+    protected Map<String, Integer> scores = null;
+    protected Boolean isActive = null;
+    protected Boolean added = null;
+
+
+
+    public ClientHandler(Socket socket, Vector moves) {
+        super();
+        this.socket = socket;
+        this.moves = moves;
+        scores = new TreeMap<>();
+        isActive = true;
+        added = false;
+        try {
+            out = new PrintWriter(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            System.err.println("IOException while opening a read/write connection");
+        }
+    }
+
+    public void run(){
+        Boolean endOfConnection = false;
+        while(!endOfConnection){
+            endOfConnection = processCommand();
+        }
+        try{
+            socket.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean processCommand(){
+        String message = null;
+        try{
+            message = in.readLine();
+        } catch (IOException e) {
+            System.err.println("Error reading command from socket");
+            return true;
+        }
+        if (null == message){
+            return true;
+        }
+        StringTokenizer st = new StringTokenizer(message);
+        String command = st.nextToken();
+        String args = null;
+        if (st.hasMoreTokens()) {
+            args = message.substring(command.length()+1, message.length());
+        }
+        return processCommand(command, args);
+    }
+
+    public boolean processCommand(String command, String args){
+        if (command.equalsIgnoreCase("SETUSR")){
+            synchronized(this){
+                scores.put(args, 0);
+                strUSR = args;
+                currentClients++;
+            }
+            out.println("200 Username Set");
+            return false;
+        } else if (command.equalsIgnoreCase("GETUSR")){
+            out.println(strUSR);
+            return false;
+        } else if (command.equalsIgnoreCase("SETMOVE")){
+            synchronized(this){
+                moves.addElement("["+strUSR+"]: "+args);
+            }
+            return false;
+        } else if (command.equalsIgnoreCase("GETMOVE")){
+            int id = (new Integer(args)).intValue();
+            String msg = null;
+            int size = moves.size();
+            if (id%2 == 0){
+                msg = (String)moves.elementAt(size-2);
+            } else {
+                msg = (String)moves.elementAt(size-1);
+            }
+            out.println(msg);
+            return false;
+        } else if (command.equalsIgnoreCase("SETWIN")) {
+            synchronized (this) {
+                if (!added){
+                    added = true;
+                } else {
+                    int wins = scores.get(args);
+                    scores.put(args, wins++);
+                    added = false;
+                }
+            }
+            return false;
+        } else if (command.equalsIgnoreCase("GETWIN")){
+            out.println(scores.get(args));
+            return false;
+        } else if (command.equalsIgnoreCase("GETCLIENTS")){
+            out.println(currentClients);
+            return false;
+        } else if (command.equalsIgnoreCase("GETSTATE")){
+            out.println(isActive);
+            return false;
+        } else if (command.equalsIgnoreCase("GETSIZE")){
+            out.println(moves.size());
+            return false;
+        } else if (command.equalsIgnoreCase("RESET")){
+            Vector moves = null; // used to store the files in the shared directory
+            int currentClients = 0;
+            String strUSR = null;
+            Map<String, Integer> scores = null;
+            Boolean isActive = false;
+            Boolean added = null;
+            return true;
+        } else if (command.equalsIgnoreCase("LOGOUT")){
+            out.println("200 Client Has Logged Out");
+            return true;
+        } else {
+            out.println("400 Unrecognized Command: "+command);
+            return false;
+        }
+    }
+}
--- src/Server/ServerTester.java
+++ src/Server/ServerTester.java
@@ -1,11 +1,41 @@
 package Server;
 
+import Client.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
 public class ServerTester {
-    public static void main(String[] args){
-        //this is where the main server tester code should go.
-        //lol idk much about this so emm this is all up to you guys :D
-        // and I'm assuming that there's a handler that you need here
-        // while the client will have a connector as well.
-        // deleted this when you actually start.
+    protected Socket clientSocket           = null; // used to store client socket
+    protected ServerSocket serverSocket     = null; // used to store server socket
+    protected int numClients                = 0; // used to keep track of number of clients
+    protected ClientHandler[] threads = null; // used for each new connection
+    protected Vector moves = new Vector();
+
+    public static int PORT = 16789; // server port
+    public static int MAX_CLIENTS = 50; // Max Clients Allowed
+
+    public ServerTester() {
+        try{
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("---------------------------");
+            System.out.println("Rock/Paper/Scissors Application is running");
+            System.out.println("---------------------------");
+            System.out.println("Listening to port: "+PORT);
+            threads = new ClientHandler[MAX_CLIENTS];
+            while(true){
+                clientSocket = serverSocket.accept(); // sets up client socket
+                System.out.println("File Sharing Client connected.");
+                threads[numClients] = new ClientHandler(clientSocket, moves); // creates new handler connection
+                threads[numClients].start(); // starts new handler connection
+                numClients++; // increments number of clients
+            }
+        } catch (IOException e) {
+            System.err.println("IOException while creating server connection");
+        }
     }
+
+    public static void main(String[] args) {ServerTester server = new ServerTester(); }
 }
